5ab26708fcac65820a5ac0deb31fda646f9c32d2
Ted Werbel <werbel.ted@gmail.com>
2017-05-29 19:07:03 -0400

    completed my first query-only graph w/ a user and company node

--- schema/schema.js
+++ schema/schema.js
@@ -4,29 +4,67 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql;
 
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: {
-    id: { type: GraphQLString } ,
-    firstName: { type: GraphQLString } ,
-    age: { type: GraphQLInt }
-  }
+// Node in our graph
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  // Arrow function used here to give access to UserType, thus resolving the closure issue
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: { // Find users who work at this company given this node's id field (parentValue.id)
+      type: new GraphQLList(UserType), // new GraphQLList() is used when returning multiple data objects (aka nodes)
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(res => res.data);
+      }
+    }
+  })
 });
 
+// Another node...
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: { // Find the company this user works at given a companyId from this user
+      type: CompanyType,
+      resolve(parentValue, args) {
+        // here we fetch data using this own node's data object (aka parentValue)
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
+      }
+    }
+  })
+});
+
+// RootQuery is a smart node that sits at the center of our graph
+// it's used to encapsulate and feed our nodes w/ data
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    user: {
-      type: UserType,
-      args: { id: { type: GraphQLString } },
-      // resolve function is used to actually obtain our data
-      resolve(parentValue, args) {
+    user: { // Find me a user given an ID
+      type: UserType, // type is used to define which node this encapsulates
+      args: { id: { type: GraphQLString } }, // args to be used for resolver
+      resolve(parentValue, args) { // resolver is used to fetch data for this node using args (aka a promise)
         return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data); // return only resp.data (an axios thing)
+      }
+    },
+    company: { // Find me a company given an ID
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
           .then(resp => resp.data);
       }
+
     }
   }
 });
