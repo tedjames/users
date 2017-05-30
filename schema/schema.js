eab21b2999e36538b29c4353e37f97d712c8fe9b
Ted Werbel <werbel.ted@gmail.com>
2017-05-29 23:48:38 -0400

    completed mutations

--- schema/schema.js
+++ schema/schema.js
@@ -85,6 +85,19 @@ const mutation = new GraphQLObjectType({
           .then(res => res.data);
       }
     },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLInt }
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+          .then(res => res.data);
+      }
+    },
     deleteUser: {
       type: UserType,
       args: {
